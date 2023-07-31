00c555540787ffe83a047361c6ca6d0203975649
enes-96 <seyit7enes@gmail.com>
2023-07-31 21:17:45 +0200

    setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = ()=>{
+  return(
+    <div>
+      <h1 className="text-3xl">i am app</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = ()=>{
+    return(
+        <header>
+            <h1>Text Analyzer</h1>
+        </header>
+    )
+}
